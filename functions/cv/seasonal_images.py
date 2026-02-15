d56c43c9b013e5e3521ed424399f7ce2f311a47b
polie94 <paulina3381@gmail.com>
2026-02-15 09:51:58 +0100

    Clean code

--- functions/cv/seasonal_images.py
+++ functions/cv/seasonal_images.py
@@ -4,51 +4,73 @@ import torch
 import operator
 import numpy as np
 
-def get_mask(img:Image) -> Image:
-    pipe = pipeline("image-segmentation")
-    results = pipe(img)
-    getter = operator.itemgetter("label")
-    result = next((result for result in results if getter(result) == "person"), None)
-    mask = result["mask"]
+from functions.cv.color_palettes import *
 
-    return mask
+SEASON_PALETTES = {
+    "autumn": autumn_colors,
+    "spring": spring_colors,
+    "summer": summer_colors,
+    "winter": winter_colors
+} 
 
-def create_palette(color_palet:dict)->Image:
-    # Image size
-    width, height = 800, 600
-    # Number of colors
-    num_colors = len(color_palet)
+class ImagePrep():
+    def __init__(self, img:Image):
+        self.image = img
+        self.season_palettes = SEASON_PALETTES
 
-    # Create a blank image
-    img = Image.new("RGB", (width, height), "#FFFFFF")  # white background
-    draw = ImageDraw.Draw(img)
+    @staticmethod
+    def _run_segmentation(img) -> dict:
+        pipe = pipeline("image-segmentation")
+        results = pipe(img)
+        return results
+        
+    def _get_mask(self) -> Image:
+        results = self._run_segmentation(self.image)
+        getter = operator.itemgetter("label")
+        result = next((result for result in results if getter(result) == "person"), None)
+        mask = result["mask"]
+        mask_array = np.array(mask)
+        mask_pil = Image.fromarray(mask_array, mode="L")
 
-    # Calculate block height
-    block_height = height // num_colors
+        return mask_pil
 
-    # Draw color blocks
-    for i, color in enumerate(color_palet):
-        top = i * block_height
-        bottom = (i + 1) * block_height
-        draw.rectangle([0, top, width, bottom], fill=color)
+    def _create_palette_bg(self, color_palette:dict) -> Image:
+        # Image size
+        width, height = 800, 600
+        # Number of colors
+        num_colors = len(color_palette)
 
-    # Optional: draw white lines between blocks for separation
-    for i in range(1, num_colors):
-        y = i * block_height
-        draw.line([0, y, width, y], fill="#FFFFFF", width=4)
+        # Create a blank image
+        img = Image.new("RGB", (width, height), "#FFFFFF")
+        draw = ImageDraw.Draw(img)
 
-    return img
+        # Calculate block height
+        block_height = height // num_colors
 
-def prepare_output(img:Image, palette:Image, mask:Image) -> Image:
+        # Draw color blocks
+        for i, color in enumerate(color_palette):
+            top = i * block_height
+            bottom = (i + 1) * block_height
+            draw.rectangle([0, top, width, bottom], fill=color)
 
-    img = img.resize(mask.size)
-    palette = palette.resize(mask.size)
-    mask_array = np.array(mask)
-    mask_pil = Image.fromarray(mask_array, mode="L")
-    palette.paste(img, (0, 0), mask_pil)
+        # Draw white lines between blocks for separation
+        for i in range(1, num_colors):
+            y = i * block_height
+            draw.line([0, y, width, y], fill="#FFFFFF", width=4)
 
-    return  palette 
-    
+        return img
+
+    def prepare_output(self) -> Image:
+
+        mask = self._get_mask()
+        img = self.image.resize(mask.size)
+        for cp_name, cp in self.season_palettes.items():
+            palette = self._create_palette_bg(cp)
+            palette = palette.resize(mask.size)
+            palette.paste(img, (0, 0), mask)
+            palette.save(f"{cp_name}.jpg")
+        
+
 
 
 
